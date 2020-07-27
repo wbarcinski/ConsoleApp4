50b5cef6758a539db4bd5eb585e854e3cabf261f
WITOLD BARCINSKI <wbarcinski@hotmail.com>
2020-07-26 21:17:31 -0500

    program replaced

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace Pluralsight.BegCShCollections.TopTenPops
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string filePath = @"C:\cs\pluralsight\csharp-collections-beginning\Pop by Largest Final.csv";
+			CsvReader reader = new CsvReader(filePath);
+
+			List<Country> countries = reader.ReadAllCountries();
+
+			foreach (Country country in countries)
+			{
+				Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+			}
+			Console.ReadKey();
+		}
+	}
 }
