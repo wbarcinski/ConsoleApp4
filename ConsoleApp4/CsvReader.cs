8e939cae8fa7984370fc7f6499c3bc2b5cb2d942
WITOLD BARCINSKI <wbarcinski@hotmail.com>
2020-07-27 09:55:22 -0500

    converted to list

--- ConsoleApp4/CsvReader.cs
+++ ConsoleApp4/CsvReader.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.Intrinsics.X86;
 
 namespace Pluralsight.BegCShCollections.TopTenPops
 {
@@ -38,12 +39,31 @@ namespace Pluralsight.BegCShCollections.TopTenPops
 		public Country ReadCountryFromCsvLine(string csvLine)
 		{
 			string[] parts = csvLine.Split(new char[] { ',' });
+			string name;
+			string code;
+			string region;
+			string popText;
 
-			string name = parts[0];
-			string code = parts[1];
-			string region = parts[2];
-			int population = int.Parse(parts[3]);
-
+            switch (parts.Length)
+            {
+				case 4:
+					name = parts[0];
+					code = parts[1];
+					region = parts[2];
+					popText = parts[3];
+					break;
+				case 5:
+					name = parts[0] + "" + parts[1];
+					name = name.Replace("\"",null).Trim();
+					code = parts[2];
+					region = parts[3];
+					popText = parts[4];
+					break;
+				default:
+					throw new Exception("can't parse  country from csvLine:"+csvLine);
+            }
+			// TryParse leaves population=0 if cant parse
+			int.TryParse(popText, out int population);
 			return new Country(name, code, region, population);
 		}
 
